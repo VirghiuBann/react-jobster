df549da4a39aaac93d56976fe0b3ceb352cde73a
virghiu <virghiu@gmail.com>
2024-05-22 09:33:15 -0400

    setup landing page

--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -1,20 +1,10 @@
-import logo from '../assets/images/logo.svg'
-import main from '../assets/images/main.svg'
+import { Link } from 'react-router-dom'
 
 const Landing = () => {
   return (
-    <main>
-      <nav>
-        <img src={logo} alt='jobster logo' className='logo' />
-      </nav>
-      <div className='info'>
-        <h1>
-          job <span>tracking</span> app
-        </h1>
-        <button className='btn btn-hero'>Login/Register</button>
-      </div>
-      <img src={main} alt='job hunt' className='img main-img' />
-    </main>
+    <Link to='/register' className='btn btn-hero'>
+      Login / Register
+    </Link>
   )
 }
 
