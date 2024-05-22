00b39f84ef471f745ea1eeec388ecbe178308d5c
virghiu <virghiu@gmail.com>
2024-05-22 10:01:19 -0400

    add styled to landing page

--- src/page/Landing.jsx
+++ src/page/Landing.jsx
@@ -1,10 +1,32 @@
 import { Link } from 'react-router-dom'
+import main from '../assets/images/main.svg'
+import Wrapper from '../assets/wrappers/LandingPage'
+import { Logo } from '../components'
 
 const Landing = () => {
   return (
-    <Link to='/register' className='btn btn-hero'>
-      Login / Register
-    </Link>
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            I'm baby butcher four loko pug cronut bicycle rights sustainable
+            hammock, post-ironic wolf. Organic cardigan jawn wayfarers beard you
+            probably haven't heard of them vaporware fashion axe 8-bit sus tacos
+            letterpress next level normcore coloring book.
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login / Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
   )
 }
 
