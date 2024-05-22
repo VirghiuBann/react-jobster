ba93cea8430d30648cc94b705f84fe7449505d72
virghiu <virghiu@gmail.com>
2024-05-22 09:23:09 -0400

    setup router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
-import Landing from './page/Landing'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { Landing, Error, Register, Dashboard } from './page'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/landing',
+    element: <Landing />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '*',
+    element: <Error />,
+  },
+])
 
 function App() {
   return (
     <>
-      <Landing />
+      <RouterProvider router={router} />
     </>
   )
 }
