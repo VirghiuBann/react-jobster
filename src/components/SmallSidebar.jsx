ab7789c099a7df613aec68304c2e37ba8e1f8d32
virghiu <virghiu@gmail.com>
2024-06-01 10:12:08 -0400

    setup SmallSidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,7 +1,23 @@
+import { FaTimes } from 'react-icons/fa'
 import Wrapper from '../assets/wrappers/SmallSidebar.js'
+import Logo from './Logo.jsx'
 
 const SmallSidebar = () => {
-  return <Wrapper>SmallSidebar</Wrapper>
+  return (
+    <Wrapper>
+      <div className='sidebar-container show-sidebar'>
+        <div className='content'>
+          <button className='close-btn' onClick={() => console.log('toggle')}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  )
 }
 
 export default SmallSidebar
