6d871c3136c7597b6cb772b1c54625e076442677
virghiu <virghiu@gmail.com>
2024-05-31 22:08:55 -0400

    create Navbar, SmallSidebar and BigSidebar component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,7 @@
-export { default as Logo } from './Logo'
-export { default as FormRow } from './FormRow'
+import Logo from './Logo'
+import FormRow from './FormRow'
+import Navbar from './Navbar'
+import SmallSidebar from './SmallSidebar'
+import BigSidebar from './BigSidebar'
+
+export { Logo, FormRow, Navbar, SmallSidebar, BigSidebar }
