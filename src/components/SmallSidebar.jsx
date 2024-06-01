3f5550dac99eee7b5c6f368e7e7f3975b2e64522
virghiu <virghiu@gmail.com>
2024-06-01 11:11:18 -0400

    refactor links to render in small and big sidebar

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -3,8 +3,7 @@ import Wrapper from '../assets/wrappers/SmallSidebar.js'
 import Logo from './Logo.jsx'
 import { useDispatch, useSelector } from 'react-redux'
 import { toggleSidebar } from '../features/user/userSlice.js'
-import links from '../utils/links'
-import { NavLink } from 'react-router-dom'
+import NavLinks from './NavLinks.jsx'
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user)
@@ -26,25 +25,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className='nav-links'>
-            {links.map((link) => {
-              const { text, path, id, icon } = link
-
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? 'nav-link active' : 'nav-link'
-                  }
-                  key={id}
-                  onClick={toggleSidebar}
-                >
-                  <span className='icon'>{icon}</span>
-                  {text}
-                </NavLink>
-              )
-            })}
-          </div>
+          <NavLinks toggleSidebar={toggle} />
         </div>
       </div>
     </Wrapper>
